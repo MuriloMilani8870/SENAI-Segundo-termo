f5969ecfc138f70775f9e9d88205fc6dc4d2c35f
Murilo Da Silva Milani <44688951876@info.local>
2019-08-22 10:09:06 -0300

    22/08/2019T10:09

---
+++ Sprint2-Backend/Exercícios/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Domains/ArtistaDomain.cs
@@ -0,0 +1,19 @@
+﻿using Senai.Sstop.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Sstop.WebApi.Controllers
+{
+    public class ArtistaDomain
+    {
+        public int IdArtista { get; set; }
+        public string Nome { get; set; }
+        //
+        public int EstiloId { get; set; }
+        public EstiloDomain Estilo { get; set; }
+
+    }
+}
---
+++ Sprint2-Backend/Exercícios/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Repositories/ArtistaRepository.cs
@@ -0,0 +1,72 @@
+﻿using Senai.Sstop.WebApi.Controllers;
+using Senai.Sstop.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Sstop.WebApi.Repositories
+{
+    public class ArtistaRepository
+    {
+        // acesso ao banco de dados
+            private string StringConexao = "Data Source= .\\SqlExpress; Initial catalog=M_SStop; User Id=sa; Pwd=132;";
+
+        public List<ArtistaDomain> Listar()
+            {
+                List<ArtistaDomain> artistas = new List<ArtistaDomain>();
+
+                using (SqlConnection con = new SqlConnection(StringConexao))
+                {
+                    string Query = "SELECT A.IdArtista, A.Nome, A.IdEstiloMusical, E.Nome AS NomeEstilo FROM Artistas A INNER JOIN EstilosMusicais E ON A.IdEstiloMusical = E.IdEstiloMusical;";
+
+                    // abre a conexao
+                    con.Open();
+
+                    SqlDataReader sdr;
+
+                    // declara o comando
+                    using (SqlCommand cmd = new SqlCommand(Query, con))
+                    {
+                        // executa a query
+                        sdr = cmd.ExecuteReader();
+
+                        while (sdr.Read())
+                        {
+                            ArtistaDomain artista = new ArtistaDomain
+                            {
+                                IdArtista = Convert.ToInt32(sdr["IdArtista"]),
+                                Nome = sdr["Nome"].ToString(),
+                                Estilo = new EstiloDomain
+                                {
+                                    IdEstilo = Convert.ToInt32(sdr["IdEstiloMusical"]),
+                                    Nome = sdr["NomeEstilo"].ToString()
+                                }
+                            };
+                            artistas.Add(artista);
+                        }
+
+                    }
+                }
+                return artistas;
+            }
+
+            public void Cadastrar(ArtistaDomain artista)
+            {
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string query = "INSERT INTO Artistas(Nome, IdEstiloMusical) VALUES (@Nome , @IdEstiloMusical)";
+
+                 SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Nome", artista.Nome);
+                cmd.Parameters.AddWithValue("@IdEstiloMusical", artista.EstiloId);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            }
+        }
+    }
+
