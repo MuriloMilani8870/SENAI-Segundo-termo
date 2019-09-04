f7d83bbfb3b80c5d3d573ae924923243e2478e3d
Murilo Da Silva Milani <44688951876@info.local>
2019-09-04 11:25:51 -0300

    04/09/2019T11:25

--- Sprint2-Backend/Exercícios/Senai.AutoPeças.WebApi/Senai.AutoPeças.WebApi/Controllers/LoginController.cs
+++ Sprint2-Backend/Exercícios/Senai.AutoPeças.WebApi/Senai.AutoPeças.WebApi/Controllers/LoginController.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Senai.AutoPeças.WebApi.Domains;
+using Senai.AutoPeças.WebApi.Repositories;
+using Senai.AutoPeças.WebApi.ViewModels;
 
 namespace Senai.AutoPeças.WebApi.Controllers
 {
@@ -12,5 +18,51 @@ namespace Senai.AutoPeças.WebApi.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        UsuarioRepository UsuarioRepository = new UsuarioRepository();
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                Usuarios Usuario = UsuarioRepository.BuscarPorEmailESenha(login);
+                if (Usuario == null)
+                    return NotFound(new { mensagem = "Email ou senha inválidos." });
+
+                var claims = new[]
+                {
+                    // email
+                    new Claim(JwtRegisteredClaimNames.Email, Usuario.Email),
+                    new Claim("chave", "valor"),
+                    // id
+                    new Claim(JwtRegisteredClaimNames.Jti, Usuario.UsuarioId.ToString()),
+                    // é a permissão do usuário
+                    new Claim(ClaimTypes.Role, Usuario.PermissaoUsuario.ToString()),
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("autopecas-chave-autenticacao"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "Inlock.WebApi",
+                    audience: "Inlock.WebApi",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                // gerar a chave pra vocês
+                // return Ok(new { mensagem = "Sucesso, bro." });
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Erro." + ex.Message });
+            }
+        }
     }
 }
